77b55367e2b6c6216ee8cacf1a8f334cd0ffa9d5
grwang91 <003213@naver.com>
2020-07-08 08:23:31 +0900

    Updates

---
+++ precache-manifest.5540b3daad6e6603bae4d742df484e26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5487914b93cc6ce2553ec7e59ba0199",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "03e4af51f92b7fa8a254",
+    "url": "/movie_app/static/css/main.659818b0.chunk.css"
+  },
+  {
+    "revision": "0745f5d4a56728403d2b",
+    "url": "/movie_app/static/js/2.aedf36e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.aedf36e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e4af51f92b7fa8a254",
+    "url": "/movie_app/static/js/main.de3c8fac.chunk.js"
+  },
+  {
+    "revision": "f7576381a1b106f30b89",
+    "url": "/movie_app/static/js/runtime-main.72826881.js"
+  }
+]);
